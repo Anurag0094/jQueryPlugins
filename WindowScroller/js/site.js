030d1b2574dc532077166176c08bcd9fd3026998
Thomas Grauer <tgrauer1@me.com>
2013-05-19 00:28:00 -0400

    remove comments

--- WindowScroller/js/site.js
+++ WindowScroller/js/site.js
@@ -1,13 +1,8 @@
-//
 $(function(){
    
-   //What fires the scroll event
    $('a').bind('click', function(){
        
-      //  $(window).scrollTop(1500);
-    
         $.jscroll({
-            //where to scroll to
             to: 'p:eq(6)',
             easing: 'easeOutBack'
             
